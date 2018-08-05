8953c98ffcf9c6fec0000ae7e0e4bba580d8475a
Ramon Ribeiro <rhpr@cesar.org.br>
2018-08-05 07:51:51 -0300

    Add missing order model

---
+++ order/order.go
@@ -0,0 +1,14 @@
+package order
+
+type Order struct {
+	OrderId            string
+	Exchange           string
+	Type               string
+	Quantity           string
+	QuantityRemaining  string
+	QuantityBaseTraded string
+	Price              string
+	Status             string
+	Created            string
+	Comments           string
+}
